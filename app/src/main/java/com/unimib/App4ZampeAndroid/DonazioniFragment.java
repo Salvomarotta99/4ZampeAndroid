173f8527cf20341c554b6880b30aa2ba9bd7ebd8
LorenzoNeglia <l.neglia@campus.unimib.it>
2021-06-03 19:16:41 +0200

    Finita pagina Donazioni con link al ENPA

--- app/src/main/java/com/unimib/App4ZampeAndroid/DonazioniFragment.java
+++ app/src/main/java/com/unimib/App4ZampeAndroid/DonazioniFragment.java
@@ -1,5 +1,7 @@
 package com.unimib.App4ZampeAndroid;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.net.URI;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,7 +28,7 @@ public class DonazioniFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private Button donationButton;
     public DonazioniFragment() {
         // Required empty public constructor
     }
@@ -55,12 +60,27 @@ public class DonazioniFragment extends Fragment {
 
 
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donazioni, container, false);
+        View v = inflater.inflate(R.layout.fragment_donazioni, container, false);
+        Button donationButton = v.findViewById(R.id.donBT);
+        donationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.comunicazioneiniziativeenpa.it/dona-ora";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+
+            }
+
+        });
+        return v;
     }
 }
